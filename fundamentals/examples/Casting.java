604f1c27c3b7e1a6abc51ca7ab0647267001364b
brianrandall <brian19892@gmail.com>
2022-11-27 15:01:59 -0800

    enough java for today. it's sunday, afterall.

---
+++ fundamentals/examples/Casting.java
@@ -0,0 +1,22 @@
+class Casting {
+    public static void main(String[] args) {
+        double a = 35.25
+        double b = 35.99
+
+        // casting the double a to an int
+        int i = (int) a;
+
+        // casting the double b to an int
+        int j = (int) b;
+
+        //depending on the context, Java will impolicity cast
+        //types for us. for example
+
+        int i = 35;
+        float f = i 
+
+        //this will return "35.0"
+
+        
+    }
+}
