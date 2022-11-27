f0402edac49835b5d2e6b8569d8a343851e37bcb
brianrandall <brian19892@gmail.com>
2022-11-27 12:14:44 -0800

    java initcommitinitiallly

---
+++ fundamentals/firstProject/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("my name is barf randorf");
+        System.out.println("i am 1000 years old");
+        System.out.println("my hometown is uranus");
+    
+    }
+}
